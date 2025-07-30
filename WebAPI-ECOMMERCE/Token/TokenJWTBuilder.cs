3995f0400ee0cb2612c82c113a3eda797972db8e
Marfernes <fernandesnunes026@gmail.com>
2025-07-30 13:08:44 -0400

    Criado a classe que gera o token

--- WebAPI-ECOMMERCE/Token/TokenJWTBuilder.cs
+++ WebAPI-ECOMMERCE/Token/TokenJWTBuilder.cs
@@ -1,15 +1,19 @@
-﻿namespace WebAPI_ECOMMERCE.Token
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace WebAPI_ECOMMERCE.Token
 {
     public class TokenJWTBuilder
     {
-        private JwtSecurityKey _jwtSecurityKey = null;
+        private SecurityKey _jwtSecurityKey = null;
         private string subject = "";
         private string issuer = "";
         private string audience = "";
         private Dictionary<string, string> claims = new Dictionary<string, string>();
         private int expiryInMinutes = 5;
 
-        public TokenJWTBuilder AddSecurityKey(JwtSecurityKey jwtSecurityKey)
+        public TokenJWTBuilder AddSecurityKey(SecurityKey jwtSecurityKey)
         {
             this._jwtSecurityKey = jwtSecurityKey;
             return this;
@@ -44,6 +48,51 @@
         {
             this.expiryInMinutes = expiryInMinutes;
             return this;
+        }
+
+        private void EnsureArguments()
+        {
+            if (this._jwtSecurityKey == null)
+            {
+                throw new ArgumentNullException("Security Key");
+            }
+
+            if (string.IsNullOrEmpty(this.subject))
+            {
+                throw new ArgumentNullException("Subject");
+            }
+
+            if (string.IsNullOrEmpty(this.issuer))
+            {
+                throw new ArgumentNullException("Issuer");
+            }
+
+            if (string.IsNullOrEmpty(this.audience))
+            {
+                throw new ArgumentNullException("Audience");
+            }
+        }
+
+        public TokenJwt Builder()
+        {
+            EnsureArguments();
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, this.subject),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            }.Union(this.claims.Select(item => new Claim(item.Key, item.Value)));
+
+            var token = new JwtSecurityToken(
+                issuer: this.issuer,
+                audience: this.audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(expiryInMinutes),
+                signingCredentials: new SigningCredentials(
+                                                      this._jwtSecurityKey,
+                                                      SecurityAlgorithms.Aes128CbcHmacSha256)
+                );
+            return new TokenJwt(token);
         }
     }
 }
